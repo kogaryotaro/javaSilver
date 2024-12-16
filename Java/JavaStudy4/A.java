6817d6ca8e97e635ab8061884bd1181d950f4805
kogaryotaro <ryotaroran531@gmail.com>
2024-12-16 22:14:29 +0900

    N0.4

---
+++ Java/JavaStudy4/A.java
@@ -0,0 +1,13 @@
+package JavaStudy4;
+
+public abstract class A {
+
+	private int id;
+	public A(int id) {
+		this.id = id;
+	}
+	
+	void sample() {
+		System.out.println("A");
+	}
+}
---
+++ Java/JavaStudy4/B.java
@@ -0,0 +1,13 @@
+package JavaStudy4;
+
+public class B extends A {
+	
+	public B(int id) {
+		super(id);
+	}
+	
+	public void sample() {
+		System.out.println("B");
+	}
+
+}
